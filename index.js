ef1b3765b2754f33cc122d15cb5e791d0b167ed3
juanfu7467v <movistarcervise@gmail.com>
2025-07-16 22:05:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
 const express = require('express');
-const axios = require('axios');
+const cors = require('cors');
+const reniecRoute = require('./routes/reniec');
+
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Rutas protegidas
+app.use('/api/reniec', reniecRoute);
+
 const PORT = process.env.PORT || 3000;
-
-// Ruta para buscar por DNI
-app.get('/reniec', async (req, res) => {
-  const dni = req.query.dni;
-  if (!dni) return res.status(400).json({ error: 'El DNI es obligatorio' });
-
-  try {
-    const response = await axios.get(`https://api.apis.net.pe/v1/dni?numero=${dni}`, {
-      headers: {
-        'Referer': 'https://apis.net.pe/api-consulta-dni.html',
-        'User-Agent': 'Mozilla/5.0'
-      }
-    });
-
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Error al obtener los datos del DNI' });
-  }
-});
-
-// Ruta de prueba
-app.get('/', (req, res) => {
-  res.send('✅ API DNI funcionando');
-});
-
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor escuchando en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
